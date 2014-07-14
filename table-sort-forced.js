58cc24d05617d902d0e510297bf41da372dff78b
James <jameshamet@gmail.com>
2014-07-14 16:03:50 -0400

    phase 2, jquery mostly removed, flash plugin still crashes

--- table-sort-forced.js
+++ table-sort-forced.js
@@ -1,7 +1,9 @@
 document.querySelector('th').onclick = function() {
     counter = 0;
+    var table = this.parentElement;
+    debugger
     while (table.tagName != "TABLE" || counter > 10) {
-      var table = this.parentElement;
+      table = this.parentElement;
     }
     if (counter > 10) {
       console.log("Failed to find nesting table");
@@ -13,12 +15,16 @@ document.querySelector('th').onclick = function() {
       if (!this.asc){rows = rows.reverse()}
       for (var i = 0; i < rows.length; i++){table.append(rows[i])}
   }
-};
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 function comparer(index) {
     return function(a, b) {
         var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+        return isNumber(valA) && isNumber(valB) ? valA - valB : valA.localeCompare(valB)
     }
 }
 function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
