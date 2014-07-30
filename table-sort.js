f9fd0274be02d6acc8d5581804243f99618b591f
James <jameshamet@gmail.com>
2014-07-30 15:13:30 -0400

    Simplified, started version 1.2

--- table-sort.js
+++ table-sort.js
@@ -1,31 +1,43 @@
-var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 
-loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", function() {
+var tableHeaders = document.querySelectorAll('th');
+var sortAsc = true;
+var sortEnabled = true;
 
+for (j = 0; j < tableHeaders.length; ++j) {
+  tableHeaders.item(j).onclick = function() {
 
-$('th').bind( "click", function() {
-    var table = $(this).parents('table').eq(0)
-    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
-    this.asc = !this.asc
-    if (!this.asc){rows = rows.reverse()}
-    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-})
+    var tableObj = findParentNode('TABLE',this);
+    var index = [].indexOf.call(this.parentNode.children, this);
+    var nodeList = tableObj.querySelectorAll('tr');
+    var nodeArray = [].slice.call(nodeList);
 
-function comparer(index) {
-    return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+    // TODO: Fix this to remove titles AND headers, right now it removes first row which is not... correct, maybe a fix is to iremove while first node has a nested <th> tag
+    nodeArray.splice(0,1);
+
+    nodeArray.sort(function(a, b) {
+      a = a.querySelectorAll('td').item(index);
+      b = b.querySelectorAll('td').item(index);
+      return a.innerHTML == b.innerHTML ? 0 : (a.innerHTML > b.innerHTML ? 1 : -1);
+    });
+
+    if (!sortAsc) {
+      nodeArray.reverse();
     }
-}
-function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
+    sortAsc = !sortAsc;
 
-});
-
-chrome.storage.local.get('OpVal', function (result0) {
-  if (result0.OpVal == false) {
-  	loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", function() {
-  		$('th').unbind('click');
-		});
+    if (sortEnabled) {
+      for (i = 0; i < nodeArray.length; ++i) {
+        tableObj.appendChild(nodeArray[i]);
+      }
+    }
   }
-});
+}
+
+function findParentNode(parentName, childObj) {
+  var testObj = childObj.parentNode;
+  while(testObj.tagName != parentName) {
+    testObj = testObj.parentNode;
+  }
+  return testObj;
+}
 
